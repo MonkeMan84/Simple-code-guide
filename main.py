985d112d571e4506c9e3abf8872df7b86e9ecd5e
Zadkiel MendezP <109963687+MonkeMan84@users.noreply.github.com>
2022-07-25 13:35:56 +0000

    Added conversion and did an excercise.

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
 #The "print" function displays whatever is in the parenthesis. Ex. Strings, variables, etc.
 #print('Hello World!')
-age = 20
-price = 19.95
+#age = 20
+#price = 19.95
+#first_name = "Josh"
+#Booleans (true or false) must have a capital letter in the begining.
+#is_online = False
+
+
+#EXCERSICE:
+#age = 20
+#first_name = "John Smith"
+#new_patient = True
+#print(age,first_name,new_patient)
+
+
+#input is for user interactions. Inputs anything typed in the terminal. If you put something before the equal sign it is considered a variable that changes with the input.
+#name = input("What is your name? ")
+#print("Hello "+ name)
+
+
+
+#Here we are using type conversion to calculate the age of the person with the birth_year variable. we used the - int(x) to change the string into an int value.
+#birth_year = input("Input your birth year")
+#age = 2022-int(birth_year)
+#print(age)
+
+
+
+#Float is a data type which is a decimal. It is like a double in java but with a different name. Ex. 10.1, 67.7
+#int()
+#float()
+#bool()
+#str()
+
+
+#EXCERCISE 2
+#first = input("First: ")
+#second = input("Second: ")
+#To Convert a string to any other kind of data type, on the math/execute line, use content from line 31-34
+#sum = float(first) + float(second)
+#print("Sum = " + str(sum))
+
+
+#String 
